a2672033b3bd69a09e4b62477b976d178721721b
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-23 12:26:11 +0300

    Creating Todos

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+
+    has_secure_password
+
+    has_many :todos
+    validates :username, {length: {minimum:5, maximum: 10}, 
+    uniqueness: true,
+    presence: true}
+
+    validates :email, {
+        uniqueness: true,
+        presence: true
+    }
+end
